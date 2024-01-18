0e6dd3b3183d84688ec73df5fcb45d2819a94f01
MutantBaby <shaheryarbakir998877@gmail.com>
2024-01-19 04:32:48 +0500

    impl virtual interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Result;
+
+use tun_tap::{Iface, Mode};
+
+extern crate tun_tap;
+
+fn main() -> Result<()> {
+    let iface0: Iface = Iface::new("tun0", Mode::Tun)?;
+    let mut buff: Vec<u8> = vec![0; 1504];
+
+    let n_bytes: usize = iface0.recv(&mut buff)?;
+
+    println!("Received {} bytes |\n{:x?}", n_bytes, &buff[..n_bytes]);
+    Ok(())
 }
