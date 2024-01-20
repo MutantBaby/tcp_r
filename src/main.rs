366db5d6dbb482fcc2461374fda38f46783df06a
MutantBaby <shaheryarbakir998877@gmail.com>
2024-01-20 16:00:45 +0500

    continuously receive & print bytes

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,10 @@ fn main() -> Result<()> {
     let iface0: Iface = Iface::new("tun0", Mode::Tun)?;
     let mut buff: Vec<u8> = vec![0; 1504];
 
-    let n_bytes: usize = iface0.recv(&mut buff)?;
+    loop {
+        let n_bytes: usize = iface0.recv(&mut buff)?;
+        println!("Received {} bytes |\n{:x?}", n_bytes, &buff[..n_bytes]);
+    }
 
-    println!("Received {} bytes |\n{:x?}", n_bytes, &buff[..n_bytes]);
     Ok(())
 }
