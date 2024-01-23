15ea7d661d094768d7d44846d28425167c8f689d
MutantBaby <shaheryarbakir998877@gmail.com>
2024-01-23 16:19:27 +0500

    parsing IPv4 packets

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,27 @@ fn main() -> Result<()> {
         let n_bytes: usize = iface0.recv(&mut buff)?;
         let flags: u16 = u16::from_be_bytes([buff[0], buff[1]]);
         let proto: u16 = u16::from_be_bytes([buff[2], buff[3]]);
-        
-        println!("Received {} bytes |\n{:x?}", n_bytes - 4, &buff[4..n_bytes]);
+
+        if proto != 0x0800 {
+            // no IPv4
+            println!("Not an IPv4 packet, skipping");
+            continue;
+        }
+
+        match etherparse::Ipv4HeaderSlice::from_slice(&buff[4..n_bytes]) {
+            Ok(packet) => {
+                println!(
+                    "Received {} bytes (Flags {:x}, \tProto {:x},) |\n{:?}",
+                    n_bytes - 4,
+                    flags,
+                    proto,
+                    packet
+                );
+            }
+            Err(e) => {
+                println!("Not an IPv4 packet, skipping: {:?}", e);
+            }
+        }
     }
 
     Ok(())
