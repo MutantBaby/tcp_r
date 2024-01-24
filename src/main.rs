c6d1d2becf23ddaa4bab1f444e92ff6244998527
MutantBaby <shaheryarbakir998877@gmail.com>
2024-01-25 00:49:26 +0500

    impl connections tracking

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use std::io::Result;
-
+use std::{collections::HashMap, io::Result, net::Ipv4Addr};
 use tun_tap::{Iface, Mode};
 
+mod tcp;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
+struct Quad {
+    src: (Ipv4Addr, u16),
+    dst: (Ipv4Addr, u16),
+}
+
 fn main() -> Result<()> {
+    
+    let mut connection: HashMap<Quad, tcp::State> = Default::default();
     let iface0: Iface = Iface::new("tun0", Mode::Tun)?;
     let mut buff: Vec<u8> = vec![0; 1504];
 
@@ -18,39 +27,41 @@ fn main() -> Result<()> {
         }
 
         match etherparse::Ipv4HeaderSlice::from_slice(&buff[4..n_bytes]) {
-            Ok(packet) => {
-                // if packet.protocol() != 0x06 {
-                //     // not TCP
-                //     println!("Not TCP packet, skipping");
-                //     continue;
-                // }
-
-                match etherparse::TcpHeaderSlice::from_slice(&buff[4 + packet.slice().len()..]) {
-                    Ok(tcp) => {
-                        println!(
-                            "SRC: {}:{}  ->  DES: {}:{}  LENGTH: {}b of TCP",
-                            packet.source_addr(),
-                            tcp.source_port(),
-                            packet.destination_addr(),
-                            tcp.destination_port(),
-                            packet.payload_len()
-                        );
-                    }
-                    Err(e) => {
-                        println!("Not a TCP packet, skipping: {:?}", e);
-                    }
+            Ok(ip_header) => {
+                if ip_header.protocol() != 0x06 {
+                    // not TCP
+                    // eprintln!("Not TCP packet, skipping");
+                    continue;
                 }
 
-                // eprintln!(
-                //     "SRC: {}  ->  DES: {}  LENGTH: {}b of PROTOCOL: {}",
-                //     packet.source_addr(),
-                //     packet.destination_addr(),
-                //     packet.payload_len(),
-                //     packet.protocol()
-                // );
+                match etherparse::TcpHeaderSlice::from_slice(&buff[4 + ip_header.slice().len()..]) {
+                    Ok(tcp_header) => {
+                        let data_i: usize = 4 + ip_header.slice().len() + tcp_header.slice().len();
+
+                        connection
+                            .entry(Quad {
+                                src: (ip_header.source_addr(), tcp_header.source_port()),
+                                dst: (ip_header.destination_addr(), tcp_header.destination_port()),
+                            })
+                            .or_default()
+                            .on_packet(ip_header.clone(), tcp_header.clone(), &buff[data_i..]);
+
+                        // println!(
+                        //     "SRC: {}:{}  ->  DES: {}:{}  LENGTH: {}b of TCP",
+                        //     ip_header.source_addr(),
+                        //     tcp_header.source_port(),
+                        //     ip_header.destination_addr(),
+                        //     tcp_header.destination_port(),
+                        //     ip_header.payload_len()
+                        // );
+                    }
+                    Err(e) => {
+                        eprintln!("Not a TCP packet, skipping: {:?}", e);
+                    }
+                }
             }
             Err(e) => {
-                println!("Not an IPv4 packet, skipping: {:?}", e);
+                eprintln!("Not an IPv4 packet, skipping: {:?}", e);
             }
         }
     }
