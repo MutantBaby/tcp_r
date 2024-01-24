130b28a74fdc3f46e02fb6798f628bf2976af8f9
MutantBaby <shaheryarbakir998877@gmail.com>
2024-01-24 23:50:33 +0500

    impl protocol

--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,35 @@ use std::io::Result;
 
 use tun_tap::{Iface, Mode};
 
-extern crate tun_tap;
-
 fn main() -> Result<()> {
     let iface0: Iface = Iface::new("tun0", Mode::Tun)?;
     let mut buff: Vec<u8> = vec![0; 1504];
 
     loop {
         let n_bytes: usize = iface0.recv(&mut buff)?;
-        let flags: u16 = u16::from_be_bytes([buff[0], buff[1]]);
-        let proto: u16 = u16::from_be_bytes([buff[2], buff[3]]);
+        let _eth_flags: u16 = u16::from_be_bytes([buff[0], buff[1]]);
+        let eth_proto: u16 = u16::from_be_bytes([buff[2], buff[3]]);
 
-        if proto != 0x0800 {
-            // no IPv4
+        if eth_proto != 0x0800 {
+            // not IPv4
             println!("Not an IPv4 packet, skipping");
             continue;
         }
 
         match etherparse::Ipv4HeaderSlice::from_slice(&buff[4..n_bytes]) {
             Ok(packet) => {
-                println!(
-                    "Received {} bytes (Flags {:x}, \tProto {:x},) |\n{:?}",
-                    n_bytes - 4,
-                    flags,
-                    proto,
-                    packet
+                if packet.protocol() != 0x06 {
+                    // not TCP
+                    println!("Not TCP packet, skipping");
+                    continue;
+                }
+
+                eprintln!(
+                    "SRC: {} -> DES: {}  LENGTH: {}b of PROTOCOL: {}",
+                    packet.source_addr(),
+                    packet.destination_addr(),
+                    packet.payload_len(),
+                    packet.protocol()
                 );
             }
             Err(e) => {
@@ -34,6 +38,4 @@ fn main() -> Result<()> {
             }
         }
     }
-
-    Ok(())
 }
